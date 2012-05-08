2ccb552996f413bccaca9877b11fa6e2ff4a8d39
Christopher Wellons <mosquitopsu@gmail.com>
2012-05-08 16:36:34 -0400

    Improve wperlin() a bit.

--- wperlin.m
+++ wperlin.m
@@ -1,6 +1,10 @@
-## Wraps perlin() so that the point can be specified as a number of
-## arguments. Handy for use with meshgrid().
-##
+## Wraps perlin() so that the point can be specified as a number of 2D
+## arguments (like perlin2d()). Handy for use with meshgrid().
 function v = wperlin(varargin)
+  v = arrayfun(@helper, varargin{:});
+end
+
+## Helps call perlin() correctly.
+function v = helper(varargin)
   v = perlin(cell2mat(varargin));
 end
