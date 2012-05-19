def5f914f44a78381543af057b3b02dea3f820db
Christopher Wellons <mosquitopsu@gmail.com>
2012-05-19 13:20:06 -0400

    Tiny bit of command line stuff.

--- src/com/nullprogram/noise/Launcher.java
+++ src/com/nullprogram/noise/Launcher.java
@@ -15,8 +15,10 @@ public final class Launcher {
         int size = 32;
         int depth = 4;
         float step = 0.04f;
-        //Runnable calc = new ClSlice3d(size, size, depth, step);
         Runnable calc = new Slice3d(size, size, depth, step);
+        if (args.length > 0 && args[0].equals("opencl")) {
+            calc = new ClSlice3d(size, size, depth, step);
+        }
         System.out.println(time(calc) + " seconds");
         System.exit(0);
     }
